cd1bddadec465e3cc7c3f3bc347d44e6aab6c750
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2026-02-04 11:22:19 +0300

    Leetcode - Missing Number

---
+++ missing_num.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        sorted_nums = sorted(nums)
+        for i in range(0, len(nums)):
+            if sorted_nums[i] != i:
+                return i
+
+        return len(nums)
