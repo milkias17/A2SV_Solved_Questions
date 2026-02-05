279ea3045acdd8e912fc2bd7ca1c3568ac3cbbdf
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2026-02-05 16:40:14 +0300

    Leetcode - Duplicates in Array 442

---
+++ duplicates_in_array.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        tmp = set()
+        res = []
+        for num in nums:
+            if num in tmp:
+                res.append(num)
+                tmp.remove(num)
+            else:
+                tmp.add(num)
+
+        return res
