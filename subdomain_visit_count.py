8730535d4d9cf796a0daf14fee2c4c8203eb38e5
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2026-02-05 16:25:10 +0300

    Leetcode - Subdomain Visit Count 811

---
+++ subdomain_visit_count.py
@@ -0,0 +1,20 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        counter = defaultdict(lambda: 0)
+
+        for domain in cpdomains:
+            count, topdomain = domain.split(" ")
+            count = int(count)
+            subdomains = topdomain.split(".")
+            for i in range(len(subdomains)):
+                counter[".".join(subdomains[i:])] += count
+
+        res = []
+        for k, v in counter.items():
+            res.append(f"{v} {k}")
+
+        return res
