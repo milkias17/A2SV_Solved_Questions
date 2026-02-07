444e8c077033d6937eb272fb4a8bc41da0896b82
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2026-02-07 21:28:50 +0300

    Leetcode - majority_element_2 229

---
+++ majority_element_2.py
@@ -0,0 +1,15 @@
+from collections import Counter
+from typing import List
+import math
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        counter = Counter(nums)
+        required = math.floor(len(nums) / 3)
+        res = []
+        for k, v in counter.items():
+            if v > required:
+                res.append(k)
+
+        return res
