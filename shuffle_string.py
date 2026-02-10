a4d903895ba115a66014c6e82e8fcedb1fc3bce9
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2026-02-10 09:06:13 +0300

    Leetcode - Shuffle String 1528

---
+++ shuffle_string.py
@@ -0,0 +1,10 @@
+from typing import List
+ 
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        new_str = [None] * len(s)
+
+        for i, idx in enumerate(indices):
+            new_str[idx] = s[i]
+
+        return "".join(new_str)
