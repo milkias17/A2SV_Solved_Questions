0b02d45f0a8ff6f2c125b2a2c6e42768f9608c78
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2026-02-10 09:03:19 +0300

    Leetcode - Two Sum 1

---
+++ two_sum.py
@@ -0,0 +1,14 @@
+from typing import List
+ 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_idx_map = {}
+        for i, num in enumerate(nums):
+            num_idx_map[num] = i
+
+        for i, num in enumerate(nums):
+            need = target - num
+            if need in num_idx_map and num_idx_map[need] != i:
+                return [i, num_idx_map[need]]
+
+        return []
