f69845019467381dcf93b7c688859f43df9bf63d
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2026-04-22 09:45:28 +0300

    Add solution for Top K Frequent Elements

---
+++ leetcode/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,16 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        freq = [[] for i in range(len(nums))]
+
+        for val, count in counter.items():
+            freq[count - 1].append(val)
+        
+        res = []
+
+        for i in range(len(freq) - 1, -1, -1):
+            res.extend(freq[i])
+            if len(res) >= k:
+                break
+        
+        return res[:k]
